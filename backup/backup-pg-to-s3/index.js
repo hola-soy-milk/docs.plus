96dc9c8c43e55ad3f26f01d12ffe7ea3f0b4f3c6
Samir Sayyad <samir.saiad@gmail.com>
2020-09-07 15:44:56 +0430

    run just

--- backup/backup-pg-to-s3/index.js
+++ backup/backup-pg-to-s3/index.js
@@ -1,13 +1,21 @@
 require('dotenv').config(); 
 
+const cron = require("node-cron");
+const express = require("express");
+const fs = require("fs");
+app = express();
 
 
+cron.schedule("* * * * *", function() {
+    console.log("running a task every minute");
+});
+app.listen(3128);
 
 ///////////////////////////////////////////////////////////
-var mainController = require("./controller/mainController");
-var controllerInstance = new mainController();
-controllerInstance.runBackup()
-.then(()=>{
-    console.log("here")
-    controllerInstance.runUpload()
-})
+// var mainController = require("./controller/mainController");
+// var controllerInstance = new mainController();
+// controllerInstance.runBackup()
+// .then(()=>{
+//     console.log("here")
+//     controllerInstance.runUpload()
+// })
